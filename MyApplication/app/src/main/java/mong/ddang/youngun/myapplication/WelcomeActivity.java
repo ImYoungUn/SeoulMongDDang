3009f3c9b39ad6c48af3545ad720b079d93db41f
Unknown <youngwoon96@naver.com>
2017-12-28 03:22:10 +0900

    첫 시작에 추천 또 하는것 넘어감

--- MyApplication/app/src/main/java/mong/ddang/youngun/myapplication/WelcomeActivity.java
+++ MyApplication/app/src/main/java/mong/ddang/youngun/myapplication/WelcomeActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.youngun.myapplication.R;
 
@@ -25,6 +26,7 @@ public class WelcomeActivity extends AppCompatActivity {
         Log.e("Welcome", "onCreate");
 
         ImageButton welcomeButton = (ImageButton) findViewById(R.id.welcomeButton);
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
         TextView welcomeComment = (TextView) findViewById(R.id.welcomeComment2);
         TextView welcomeComment2 = (TextView) findViewById(R.id.welcomeComment5);
 
@@ -33,6 +35,7 @@ public class WelcomeActivity extends AppCompatActivity {
         String comm = name + "님의 성향을 먼저 알아보겠습니다.";
         String comm2 = "혹시 보고싶은 컨텐츠가 있으면 '찜해놓기!'를 눌러 저장하셔야 해요\n 평가를 한 컨텐츠는 다시 추천해 드리지 않습니다. \n-컨텐츠들이 너어어무 많거든요...!";
 
+        welcomeText.setSelected(true);
         welcomeComment.setText(comm);
         welcomeComment2.setText(comm2);
 
@@ -40,9 +43,15 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //NewUser는 시작을 하기 위해 고객의 mongId를 발급받는 작업이 완료 되었는지를 검사.
-                if (ReadyForGetMongId.NewUser != null) {
+
+                if (ReadyForGetMongId.NewUser.compareTo("first")==0) {
                     Intent intent = new Intent(wActivity, WelcomeRatingDialog.class);
                     startActivityForResult(intent,1);
+                }else if(ReadyForGetMongId.NewUser.compareTo("rated")==0){
+                    Toast.makeText(getApplicationContext(), "이미 다 하셨었군요! 추천페이지로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                    Intent intent1 = new Intent(wActivity, ReadyForGetMongId.class);
+                    startActivity(intent1);
+                    finish();
                 }
             }
         });
